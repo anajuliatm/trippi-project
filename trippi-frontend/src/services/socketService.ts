97473f136d285d05db6b2032138b484e25079b5b
Ana Julia <57350939+anajuliatm@users.noreply.github.com>
2026-06-17 17:35:09 -0300

    CORS Error: faltou liberar o Render no socketService

--- trippi-frontend/src/services/socketService.ts
+++ trippi-frontend/src/services/socketService.ts
@@ -1,13 +1,13 @@
 import { io, type Socket } from "socket.io-client";
 
-//const SOCKET_URL = import.meta.env.VITE_API_URL ?? "http://localhost:8000";
+const SOCKET_URL = import.meta.env.VITE_API_URL ?? "http://localhost:8000";
 
 
 let socket: Socket | null = null;
 
 export function getSocket(): Socket {
   if (!socket) {
-    socket = io("http://localhost:8000", { transports: ["websocket"] });
+    socket = io(SOCKET_URL, { transports: ["websocket"] });
   }
   return socket;
 }
