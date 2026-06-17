8ddd89e60a5e2210038f3a44d2cfe27ce6e977e6
Ana Julia <57350939+anajuliatm@users.noreply.github.com>
2026-06-17 13:22:29 -0300

    Render liberado no CORS - para o deploy funcionar

--- trippi-backend/app/main.py
+++ trippi-backend/app/main.py
@@ -17,6 +17,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://trippi-project-front.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
