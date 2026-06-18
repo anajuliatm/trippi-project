350fbb2c3bb3c1d2b822d7391d8f68f86ec8d1e2
Ana Julia Ticianeli <anajuliamarcelino02@gmail.com>
2026-06-18 17:57:51 -0300

    Configuracoes para a aplicacao rodar no docker

--- trippi-backend/app/main.py
+++ trippi-backend/app/main.py
@@ -17,7 +17,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://trippi-project-front.onrender.com"
+        "https://trippi-project-front.onrender.com",
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
